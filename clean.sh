8d89c6fc8a657c414d23ef4e13bd25265d4ff179
tvaughan <tvaughan@f3b90fed-20f5-0310-8406-f4dde2dd57b2>
2008-03-19 04:53:13 +0000

    kochanski is also required by kryten...
    git-svn-id: file:///home/tvaughan/Projects/svn/svn/trunk@17 f3b90fed-20f5-0310-8406-f4dde2dd57b2

--- clean.sh
+++ clean.sh
@@ -3,6 +3,11 @@
 KRYTEN_HOME=$(dirname $0)
 . ${KRYTEN_HOME}/etc/common
 
+for x in ${KRYTEN_EXTRAS}; do
+	${KRYTEN_HOME}/extras/${x}/clean.sh $*
+	[ $? != 0 ] && echo "ERROR!!!" && exit 1
+done
+
 cd ${KRYTEN_HOME}
 
 TARGET="$1"
