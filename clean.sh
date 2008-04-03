e8c720b26b04689d533212757a7de5b541f2ba39
tvaughan <tvaughan@f3b90fed-20f5-0310-8406-f4dde2dd57b2>
2008-04-03 20:45:18 +0000

    oop
    git-svn-id: file:///home/tvaughan/Projects/svn/svn/trunk@53 f3b90fed-20f5-0310-8406-f4dde2dd57b2

--- clean.sh
+++ clean.sh
@@ -4,7 +4,7 @@ GRIMA_HOME=$(dirname $0)
 . ${GRIMA_HOME}/etc/common
 
 for x in ${GRIMA_EXTRAS}; do
-	${GRIMA_HOME}/extras/${x}/clean.sh $*
+	${GRIMA_HOME}/extras/${x}/clean.sh "$@"
 	[ $? != 0 ] && echo "ERROR!!!" && exit 1
 done
 
