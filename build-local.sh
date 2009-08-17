0f4e565e0f616cb09319e08df406d91d48a4699b
tvaughan <tvaughan@f3b90fed-20f5-0310-8406-f4dde2dd57b2>
2009-08-17 20:55:50 +0000

    so that other non-interactive tools may invoke a build
    git-svn-id: file:///home/tvaughan/Projects/svn/svn/trunk@148 f3b90fed-20f5-0310-8406-f4dde2dd57b2

--- build-local.sh
+++ build-local.sh
@@ -3,13 +3,15 @@
 GRIMA_HOME=$(dirname $0)
 . ${GRIMA_HOME}/etc/common
 
-echo -n "Delete the database? [y/N] "
-read answer
-case "${answer}" in
-	[yY]*)
-                grima-db-delete.sh
-	;;
-esac
+if [ ! "${SKIP_QUESTIONS}" = "" ]; then
+        echo -n "Delete the database? [y/N] "
+        read answer
+        case "${answer}" in
+                [yY]*)
+                        grima-db-delete.sh
+                ;;
+        esac
+fi
 
 grima-db-load.sh
 # TODO: [ $? != 0 ] && exit 1
