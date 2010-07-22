f3b8b2f9dcade6508976b049c3346304bdcae812
Tom Vaughan <tom@software6.net>
2010-07-22 08:10:17 -0700

    obsolete

--- bin/svn.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-GRIMA_HOME=$(dirname $0)/..
-export GRIMA_HOME
-
-. ${GRIMA_HOME}/etc/common
-
-grima_record ${SVN} ${SVNFLAGS} ${GRIMA_SVNFLAGS} "$@"
-exec ${SVN} ${SVNFLAGS} ${GRIMA_SVNFLAGS} "$@"
