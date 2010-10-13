02c352b824e66d3741471f398501aa4a818bc435
Tom Vaughan <tom@software6.net>
2010-10-13 11:22:13 -0700

    java is obsolete...

--- bin/java.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-GRIMA_HOME=$(dirname $0)/..
-export GRIMA_HOME
-
-. ${GRIMA_HOME}/etc/common
-
-grima_record ${JAVA} ${JAVAFLAGS} ${GRIMA_JAVAFLAGS} "$@"
-exec ${JAVA} ${JAVAFLAGS} ${GRIMA_JAVAFLAGS} "$@"
