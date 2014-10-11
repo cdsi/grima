7e691e3e7eb94ca947e46e92bee32857daa3587b
Tom Vaughan <tvaughan@tocino.cl>
2014-10-10 22:24:30 -0300

    Port to Ubuntu 14.04.

--- bin/grima-django-manage.py
+++ bin/grima-django-manage.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
-from grima.www.manage import *
+import sys
 
-execute_manager(settings)
+from grima.www.manage import execute_from_command_line
+
+execute_from_command_line(sys.argv)
 
 # $Id:$
 #
